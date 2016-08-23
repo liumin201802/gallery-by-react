6f868059464fd43a9be76003a32d94dd96b93fca
liumin <liumin@haohandata.com.cn>
2016-08-23 15:50:10 +0800

    init project structure

---
+++ src/components/Main.js
@@ -0,0 +1,38 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+//获取图片相关的数据
+// var imageDatas = require('../data/imageDatas.json');
+// // let imageDatas = require('json!../data/imageDatas.json');
+
+// //利用自执行函数，将图片名信息转成图片URL路径信息
+// imageDatas = ((imageDatasArr)=> {
+//   for (var i = 0, j = imageDatasArr.length; i < j; i++) {
+//     let singleImageData = imageDatasArr[i];
+//     singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+//     imageDatasArr[i] = singleImageData;
+//   }
+//   return imageDatasArr;
+// })(imageDatas);
+
+import React from 'react';
+
+// let yeomanImage = require('../images/yeoman.png');
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section className="stage">
+      	<section className="img-sec">
+      	</section>
+      	<nav className="controller-nav">
+      	</nav>
+      </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
